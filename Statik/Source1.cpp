dbab4a25fafe8d81eb27257b99fbcb083abb58d7
IndraHfid <indrahafidsaputra@gmail.com>
2024-05-14 15:24:16 +0700

    Menginisialisasi atribut Nim dan Nama'dalam konstruktor 'mahasiswa'

---
+++ Statik/Source1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+public:
+    int nim;
+    string nama;
+
+public:
+
+    mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    }
+};
